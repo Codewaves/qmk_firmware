55f513c9b9c32727c5c782defa2beb70ce2d5db7
Sergej Kravcenko <skravcenko@codewaves.com>
2020-08-23 01:31:38 +0300

    Optimize punctuations

--- keyboards/clicky/keymaps/default/keymap.c
+++ keyboards/clicky/keymaps/default/keymap.c
@@ -50,6 +50,7 @@ enum custom_keycodes {
 #define CL_CUT LCTL(KC_X)
 #define CL_COPY LCTL(KC_C)
 #define CL_PSTE LCTL(KC_V)
+#define CL_UNDO LCTL(KC_Z)
 
 #define ADJUST LT(_ADJUST, KC_NO)
 #define RGB_TGL KC_TRNS     
@@ -59,30 +60,30 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT(\
-    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_ESC, \
+    KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_ESC, \
     KC_LSFT, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,         KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_RSFT, \
     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, \
                                KC_BSPC, SPC_LWR, TAB_LAL,      ENT_SFT, SPC_RSE, KC_DEL \
 ),
 
 [_QWERTY] = LAYOUT(\
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC, \
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC, \
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT, \
     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, \
                                KC_BSPC, SPC_LWR, TAB_LAL,      ENT_SFT, SPC_RSE, KC_DEL \
 ),
 
 [_LOWER] = LAYOUT(\
-    _______, KC_LGUI, KC_PGUP, KC_UP,   KC_PGDN, KC_INS,       KC_MINS, KC_7,    KC_8,    KC_9,    KC_PAST, _______, \
-    _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,       KC_EQL,  KC_4,    KC_5,    KC_6,    KC_PSLS, _______, \
-    _______, KC_NLCK, CL_CUT,  CL_COPY, CL_PSTE, KC_CAPS,      KC_PPLS, KC_1,    KC_2,    KC_3,    KC_DOT,  _______, \
+    _______, KC_LGUI, KC_PGUP, KC_UP,   KC_PGDN, KC_INS,       KC_MINS, KC_7,    KC_8,    KC_9,    KC_ASTR, _______, \
+    _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,       KC_EQL,  KC_4,    KC_5,    KC_6,    KC_SLSH, _______, \
+    _______, CL_UNDO, CL_CUT,  CL_COPY, CL_PSTE, KC_CAPS,      KC_PLUS, KC_1,    KC_2,    KC_3,    KC_DOT,  _______, \
                                _______, _______, _______,      _______, _______, KC_0\
 ),
 
 [_RAISE] = LAYOUT(\
-    _______, _______, KC_CIRC, KC_EXLM, KC_DQUO, KC_QUOT,      KC_ASTR, KC_LBRC, KC_AMPR, KC_RBRC, _______, _______, \
-    _______, _______, KC_LABK, KC_PERC, KC_RABK, KC_TILD,      KC_QUES, KC_LPRN, KC_DLR,  KC_RPRN, KC_BSLS, _______, \
-    _______, _______, _______, KC_AT,   KC_UNDS, KC_GRV,       KC_PIPE, KC_LCBR, KC_HASH, KC_RCBR, _______, _______, \
+    _______, KC_TILD, KC_EQL,  KC_EXLM, KC_DQUO, KC_QUOT,      KC_ASTR, KC_LBRC, KC_AMPR, KC_RBRC, _______, _______, \
+    _______, KC_GRV,  KC_LABK, KC_PERC, KC_RABK, KC_PLUS,      KC_QUES, KC_LPRN, KC_DLR,  KC_RPRN, KC_BSLS, _______, \
+    _______, _______, KC_CIRC, KC_AT,   KC_UNDS, KC_MINS,      KC_PIPE, KC_LCBR, KC_HASH, KC_RCBR, _______, _______, \
                                _______, _______, _______,      _______, _______, _______\
 ),
 
